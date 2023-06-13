240964072670f30be71880cad3411f1b44c58bcf
abdulhadi <abdulhadi08022003@gmail.com>
2023-06-13 09:56:09 +0800

    Membuat class Mahasiswa dan MahasiswaBeraksi

---
+++ src/universitas/MahasiswaBeraksi.java
@@ -0,0 +1,13 @@
+
+package universitas;
+public class MahasiswaBeraksi {
+    public static void main(String[] args) {
+        Mahasiswa hadi = new Mahasiswa ("2110010198","Hadi","TI");
+        System.out.println("Nama Mahasiswa = "+ hadi.getNama());
+        System.out.println("Npm Mahasiswa = "+ hadi.getNpm());
+        System.out.println("Prodi Mahasiswa = "+ hadi.getProdi());
+        
+        
+    }
+    
+}
